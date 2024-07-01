a7583ce6d99128aa33bf7436d966ba6676097f5d
Mingxun Wang <mwang87@gmail.com>
2024-07-01 14:40:24 -0700

    adding debug

--- run_worker_conversion.sh
+++ run_worker_conversion.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-celery -A tasks_conversion worker --autoscale=8,1 -l info -B -Q conversion --max-tasks-per-child 10
+celery -A tasks_conversion worker --autoscale=8,1 -l debug -B -Q conversion --max-tasks-per-child 10
